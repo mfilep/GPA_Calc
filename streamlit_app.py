6dfd3e76a8a0bf48ff64fb31f4681cda869581c1
mfilep <118207571+mfilep@users.noreply.github.com>
2024-03-01 08:41:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ honors = {"A": 5, "B": 4, "C": 3, "D": 2, "F": 1}
 df = pd.DataFrame(
     [
         {"Year": ['9', '10', '11', '12'],
-         "Semester": ['1', '2']
+         "Semester": ['1', '2'],
          "Class Name": "Enter Class Name",
          "Grade": ['A', 'B', 'C', 'D', 'F'],
          "Converted": "number",
