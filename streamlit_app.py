b11c5a872d2b2a529601cf7a30f0409d1832c915
mfilep <118207571+mfilep@users.noreply.github.com>
2024-03-01 09:02:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,16 +20,15 @@ df = pd.DataFrame(
          "Grade": ['A', 'B', 'C', 'D', 'F'],
          "Converted": "number",
          "Honors?": False,
-         "AP?": False},
-    
-    ])
+         "AP?": False}        
+     ], dtype = "category")
 
 #df = load_data()
 
 edited_df = st.data_editor(df,
                            column_order = ["Year", "Semester", "Class Name", "Honors?", "AP?", "Letter Grade", "Converted"],
                            column_config = {"Year": st.column_config.SelectboxColumn("Year", options = ['9', '10', '11', '12']),
-                                            "Semester": st.column_config.SelectboxColumn("Semester", options= ['1', '2'])
+                                            "Semester": st.column_config.SelectboxColumn("Semester", options= ['1', '2']),
                                             "Letter Grade": st.column_config.SelectboxColumn("Grade", options= ['A', 'B', 'C', 'D', 'F'])
                                            }                                                                         
                            num_rows="dynamic") #creates an editable form
